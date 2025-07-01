fae92fa329d3239cac3b7133f28b274c0ee14260
Hazem <110736955+Hazem-Elshahawy@users.noreply.github.com>
2025-07-01 16:10:54 +0300

    Initial Commit

--- src/test/java/DDG_titleCheck.java
+++ src/test/java/DDG_titleCheck.java
@@ -1,10 +1,15 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class DDG_titleCheck {
 
@@ -25,17 +30,28 @@ public class DDG_titleCheck {
         driver.manage().window().maximize();
     }
 
+
+    //Title Checking
     @Test
-    public void titleCheck() {
+    public void T1titleCheck(){
         String actualTitle = driver.getTitle();
         String expectedTitle = "Google";
 
         Assert.assertEquals(actualTitle, expectedTitle);
 
         System.out.println("There is a bug as the actual is (" + actualTitle + ") and the expected is " + expectedTitle);
-
     }
 
+
+    //Logo Checker
+    @Test
+    public void T2logoExist(){
+        WebElement logo = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/article/div[1]/div[1]/div[2]/div/header/div/section[1]/a/img"));
+
+        Assert.assertTrue(logo.isDisplayed(), "DuckDuckGo Logo is not displayed");
+    }
+
+    
     @AfterClass
     public void quit() throws InterruptedException {
         Thread.sleep(2000);
